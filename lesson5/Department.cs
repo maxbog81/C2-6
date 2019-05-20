350d98ccc112f915621f5f1f7dfa0aa8fdccbbc9
Maksim Bogatov <maxbog@inbox.ru>
2019-05-20 18:25:38 +0300

    Урок 6

---
+++ lesson5/Department.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Department : INotifyPropertyChanged
+    {        
+        public ObservableCollection<Department> ListDept = new ObservableCollection<Department>();
+        private string dept;
+        public string Dept
+        {
+            get { return this.dept; }
+            set
+            {
+                if (this.dept != value)
+                {
+                    this.dept = value;
+                    this.NotifyPropertyChanged("Dept");
+                }
+            }
+        }
+        public Department(string dept)
+        {
+            Dept = dept;
+        }
+
+        public Department()
+        {
+        }
+
+        public void AddElement()
+        {
+            ListDept.Add(new Department($"Подразд_100"));
+            ListDept.Add(new Department($"Подразд_101"));
+            ListDept.Add(new Department($"Подразд_102"));
+            ListDept.Add(new Department($"Подразд_103"));
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+        //public override string ToString()
+        //{
+        //    return Dept;
+        //}
+    }
+}
---
+++ lesson5/Employee.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lesson6
+{
+    class Employee : INotifyPropertyChanged
+    {
+        Random r = new Random();
+        public ObservableCollection<Employee> ListEmp = new ObservableCollection<Employee>();
+        private string name;
+        private string dept;
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    this.NotifyPropertyChanged("Name");
+                }
+            }
+        }
+
+        public string Dept
+        {
+            get { return this.dept; }
+            set
+            {
+                if (this.dept != value)
+                {
+                    this.dept = value;
+                    this.NotifyPropertyChanged("Dept");
+                }
+            }
+        }
+        public Employee(string name,string dept)
+        {
+            Name = name;
+            Dept = dept;
+        }
+
+        public Employee()
+        {
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void NotifyPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+
+
+        public void AddElement()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                ListEmp.Add(new Employee($"Имя_{i}", $"Подразд_{r.Next(100, 103)}"));
+            }
+        }
+
+        //public override string ToString()
+        //{
+        //    return Name+" - " +Dept;
+        //}
+    }
+}
